6a4cd8ab2d80eab2524fee06150cc80a3a853737
Tim <thcoomer@hotmail.com>
2016-09-09 10:17:34 +1200

    added delete words method

--- data/db.js
+++ data/db.js
@@ -3,11 +3,13 @@ var path = require('path')
 
 module.exports = {
   getWords: getWords,
-  saveWord: saveWord
+  saveWord: saveWord,
+  deleteWords: deleteWords
 }
 
 var wordsFile = path.join(__dirname, 'words.json')
 
+// Method to show all current words
 function getWords(cb) {
   fs.readFile(wordsFile, function (err, buffer) {
     if (err) {
@@ -32,7 +34,25 @@ function saveWord(word, callback) {
 
     fs.writeFile(wordsFile, jsonString, function (err) {
       cb(err)
-      console.log
+      console.log(`${word} added to file`)
+    })
+  })
+}
+
+// Method to clear the page of all words
+function deleteWords(words, callback) {
+  fs.readFile(wordsFile, function (err, callback) {
+    if (err) {
+      return cb(err)
+    }
+
+    var words = Json.parse(buffer)
+    words = []
+    var jsonString = JSON.stringify(words)
+
+    fs.writeFile(wordsFile, function (err, callback) {
+      cb(err)
+      console.log("words deleted")
     })
   })
 }
